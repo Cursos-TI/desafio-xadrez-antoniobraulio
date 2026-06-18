d09507a1f934f0abd539f62d9b6dba6255f2b7bc
antoniobraulio <antonio.braulio@outlook.com>
2026-06-18 10:02:57 +0000

    Desafio Novato concluido

--- xadrez.c
+++ xadrez.c
@@ -17,6 +17,278 @@ int main() {
     // Implementação de Movimentação da Rainha
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
 
+    //Declaracao e inicializacao das variaveis
+    char* nomePeca = "Peca a mover";
+    int paraCima = 0;
+    int paraBaixo = 0;
+    int paraDireita = 0;
+    int paraEsquerda = 0;
+    int paraCimaDireita = 0;
+    int paraCimaEsquerda = 0;
+    int paraBaixoDireita = 0;
+    int paraBaixoEsquerda = 0;
+    int opcaoPeca = 0;
+    int opcaoMovimeto = 0;
+
+    //Menu para escolha da peca a mover
+    do
+    {
+        //Definicao da peca a ser movimentada
+        printf("\nSelecione a peca a ser movimentada:\n");
+        printf("1 - Rainha\n");
+        printf("2 - Torre\n");
+        printf("3 - Bispo\n");
+        printf("4 - Sair do jogo\n");
+        printf("Opcao: ");
+        scanf("%d",&opcaoPeca);
+    
+        //Definicao da direcao e sentido do movimento
+        switch (opcaoPeca)
+        {
+            case 1:
+                do
+                {
+                    printf("\nSelecione a direcao e sentido do movimento da Rainha:\n");
+                    printf("1 - Para cima\n");
+                    printf("2 - Para baixo\n");
+                    printf("3 - Para a direita\n");
+                    printf("4 - Para a esquerda\n");
+                    printf("5 - Na diagonal para cima e a direita\n");
+                    printf("6 - Na diagonal para cima e a esquerda\n");
+                    printf("7 - Na diagonal para baixo e a direita\n");
+                    printf("8 - Na diagonal para baixo e a esquerda\n");
+                    printf("9 - Escolher outra peca\n");
+                    printf("Opcao: ");
+                    scanf("%d",&opcaoMovimeto);
+
+                    //Definicao da quantidade de movimento
+                    if(opcaoMovimeto < 9)
+                    {
+                        printf("\nInforme o numero de 'casas' a mover: ");
+                    }
+                    switch (opcaoMovimeto)
+                    {
+                        case 1:
+                            scanf("%d",&paraCima);
+                            printf("\n");
+                            for(int i = 1; i <= paraCima; i++)
+                            {
+                                printf("Rainha move para cima\n");
+                            }
+                                //opcaoMovimeto = 0;
+                            break;
+                        case 2:
+                            scanf("%d",&paraBaixo);
+                            printf("\n");
+                            for(int i = 1; i <= paraBaixo; i++)
+                            {
+                                printf("Rainha move para baixo\n");
+                            }
+                            break;
+                        case 3:
+                            scanf("%d",&paraDireita);
+                            printf("\n");
+                            for(int i = 1; i <= paraDireita; i++)
+                            {
+                                printf("Rainha move para direita\n");
+                            }
+                            break;
+                        case 4:
+                            scanf("%d",&paraEsquerda);
+                            printf("\n");
+                            for(int i = 1; i <= paraEsquerda; i++)
+                            {
+                                printf("Rainha move para esqueda\n");
+                            }
+                            break;
+                        case 5:
+                            scanf("%d",&paraCimaDireita);
+                            printf("\n");
+                            for(int i = 1; i <= paraCimaDireita; i++)
+                            {
+                                printf("Rainha move para cima e a direita\n");
+                            }
+                            break;
+                        case 6:
+                            scanf("%d",&paraCimaEsquerda);
+                            printf("\n");
+                            for(int i = 1; i <= paraCimaEsquerda; i++)
+                            {
+                                printf("Rainha move para cima e a esquerda\n");
+                            }
+                            break;
+                        case 7:
+                            scanf("%d",&paraBaixoDireita);
+                            printf("\n");
+                            for(int i = 1; i <= paraBaixoDireita; i++)
+                            {
+                                printf("Rainha move para baixo e a direita\n");
+                            }
+                            break;
+                        case 8:
+                            scanf("%d",&paraBaixoEsquerda);
+                            printf("\n");
+                            for(int i = 1; i <= paraBaixoEsquerda; i++)
+                            {
+                                printf("Rainha move para baixo e a esquerda\n");
+                            }
+                            break;
+                        case 9:
+                            printf("Escolha outra peca.");
+                            opcaoMovimeto = 0;
+                            opcaoPeca = 5;
+                            break;           
+                        default:
+                            printf("Escolha errada, deve ser entre 1 e 9.\n");
+                            opcaoMovimeto = 10;
+                            opcaoPeca = 5;
+                            break;
+                    }
+                } while (opcaoMovimeto != 0);
+                break;
+            case 2:
+                do
+                {
+                    printf("\nSelecione a direcao e sentido do movimento da Torre:\n");
+                    printf("1 - Para cima\n");
+                    printf("2 - Para baixo\n");
+                    printf("3 - Para a direita\n");
+                    printf("4 - Para a esquerda\n");
+                    printf("5 - Escolher outra peca\n");
+                    printf("Opcao: ");
+                    scanf("%d",&opcaoMovimeto);
+
+                    //Definicao da quantidade de movimento
+                    if(opcaoMovimeto < 5)
+                    {
+                        printf("\nInforme o numero de 'casas' a mover: ");
+                    }
+                    switch (opcaoMovimeto)
+                    {
+                        case 1:
+                            scanf("%d",&paraCima);
+                            printf("\n");
+                            for(int i = 1; i <= paraCima; i++)
+                            {
+                                printf("Torre move para cima\n");
+                            }
+                                //opcaoMovimeto = 0;
+                            break;
+                        case 2:
+                            scanf("%d",&paraBaixo);
+                            printf("\n");
+                            for(int i = 1; i <= paraBaixo; i++)
+                            {
+                                printf("Torre move para baixo\n");
+                            }
+                            break;
+                        case 3:
+                            scanf("%d",&paraDireita);
+                            printf("\n");
+                            for(int i = 1; i <= paraDireita; i++)
+                            {
+                                printf("Torre move para direita\n");
+                            }
+                            break;
+                        case 4:
+                            scanf("%d",&paraEsquerda);
+                            printf("\n");
+                            for(int i = 1; i <= paraEsquerda; i++)
+                            {
+                                printf("Rainha move para esqueda\n");
+                            }
+                            break;
+                        case 5:
+                            printf("Escolha outra peca.");
+                            opcaoMovimeto = 0;
+                            opcaoPeca = 5;
+                            break;           
+                        default:
+                            printf("Escolha errada, deve ser entre 1 e 5.\n");
+                            opcaoMovimeto = 6;
+                            opcaoPeca = 5;
+                            break;
+                    }
+                } while (opcaoMovimeto != 0);
+                break;
+            case 3:
+                do
+                {
+                    printf("\nSelecione a direcao e sentido do movimento do Bispo:\n");
+                    printf("1 - Na diagonal para cima e a direita\n");
+                    printf("2 - Na diagonal para cima e a esquerda\n");
+                    printf("3 - Na diagonal para baixo e a direita\n");
+                    printf("4 - Na diagonal para baixo e a esquerda\n");
+                    printf("5 - Escolher outra peca\n");
+                    printf("Opcao: ");
+                    scanf("%d",&opcaoMovimeto);
+
+                    //Definicao da quantidade de movimento
+                    if(opcaoMovimeto < 5)
+                    {
+                        printf("\nInforme o numero de 'casas' a mover: ");
+                    }
+                    switch (opcaoMovimeto)
+                    {
+                        case 1:
+                            scanf("%d",&paraCimaDireita);
+                            printf("\n");
+                            for(int i = 1; i <= paraCimaDireita; i++)
+                            {
+                                printf("Bispo move para cima e a direita\n");
+                            }
+                            break;
+                        case 2:
+                            scanf("%d",&paraCimaEsquerda);
+                            printf("\n");
+                            for(int i = 1; i <= paraCimaEsquerda; i++)
+                            {
+                                printf("Bispo move para cima e a esquerda\n");
+                            }
+                            break;
+                        case 3:
+                            scanf("%d",&paraBaixoDireita);
+                            printf("\n");
+                            for(int i = 1; i <= paraBaixoDireita; i++)
+                            {
+                                printf("Bispo move para baixo e a direita\n");
+                            }
+                            break;
+                        case 4:
+                            scanf("%d",&paraBaixoEsquerda);
+                            printf("\n");
+                            for(int i = 1; i <= paraBaixoEsquerda; i++)
+                            {
+                                printf("Bispo move para baixo e a esquerda\n");
+                            }
+                            break;
+                        case 5:
+                            printf("Escolha outra peca.");
+                            opcaoMovimeto = 0;
+                            opcaoPeca = 5;
+                            break;           
+                        default:
+                            printf("Escolha errada, deve ser entre 1 e 5.\n");
+                            opcaoMovimeto = 6;
+                            opcaoPeca = 5;
+                            break;
+                    }
+                } while (opcaoMovimeto != 0);
+                break;
+            case 4:
+                printf("Voce escolheu sair do jogo.\n");
+                opcaoPeca = 0;
+                break;
+            default:
+                printf("Escolha errada, deve ser entre 1 e 4.\n");
+                opcaoPeca = 0;
+                break;
+        }
+    } while (opcaoPeca != 0);
+
+    printf("Jogo encerrado.\n");
+
+
     // Nível Aventureiro - Movimentação do Cavalo
     // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
     // Um loop pode representar a movimentação horizontal e outro vertical.
