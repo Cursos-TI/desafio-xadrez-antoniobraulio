bd50cba1b685f0b6b2d49030a4fa1c094834af9e
antoniobraulio <antonio.braulio@outlook.com>
2026-06-18 21:34:40 +0000

    Desafio Mestre concluido

--- xadrez.c
+++ xadrez.c
@@ -4,6 +4,29 @@
 // Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
 
+//Metodo para movimentacao de pecas
+void movePeca(int numCasas, char* frase)
+{
+    //Empregando a recursividade
+    printf("\n");
+    if(numCasas > 0)
+    {
+        printf(frase);
+        movePeca(numCasas - 1, frase);
+    }
+
+    //Opcao de utilizar o loop for
+    /*
+    printf("\n");
+    for(int i = 1; i <= numCasas; i++)
+    {
+        printf(frase);
+        printf("\n");
+    }
+    */
+}
+
+//Programa principal
 int main() {
     // Nível Novato - Movimentação das Peças
     // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
@@ -22,6 +45,15 @@ int main() {
     // Nível Aventureiro - Movimentação do Cavalo
     // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
     // Um loop pode representar a movimentação horizontal e outro vertical.
+
+    //     NIVEL AVENTUREIRO CONCLUIDO
+
+    // Nível Mestre - Funções Recursivas e Loops Aninhados
+    // Sugestão: Substitua as movimentações das peças por funções recursivas.
+    // Exemplo: Crie uma função recursiva para o movimento do Bispo.
+
+    // Sugestão: Implemente a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.
+    // Inclua o uso de continue e break dentro dos loops.
     
     //Declaracao e inicializacao das variaveis
     char* nomePeca = "Peca a mover";
@@ -81,71 +113,38 @@ int main() {
                     {
                         case 1:
                             scanf("%d",&paraCima);
-                            printf("\n");
-                            for(int i = 1; i <= paraCima; i++)
-                            {
-                                printf("Rainha move para cima\n");
-                            }
-                                //opcaoMovimeto = 0;
+                            movePeca(paraCima, "Rainha move para cima");
                             break;
                         case 2:
                             scanf("%d",&paraBaixo);
-                            printf("\n");
-                            for(int i = 1; i <= paraBaixo; i++)
-                            {
-                                printf("Rainha move para baixo\n");
-                            }
+                            movePeca(paraBaixo, "Rainha move para baixo");
                             break;
                         case 3:
                             scanf("%d",&paraDireita);
-                            printf("\n");
-                            for(int i = 1; i <= paraDireita; i++)
-                            {
-                                printf("Rainha move para direita\n");
-                            }
+                            movePeca(paraDireita, "Rainha move para direita");
                             break;
                         case 4:
                             scanf("%d",&paraEsquerda);
-                            printf("\n");
-                            for(int i = 1; i <= paraEsquerda; i++)
-                            {
-                                printf("Rainha move para esqueda\n");
-                            }
+                            movePeca(paraEsquerda, "Rainha move para esqueda");
                             break;
                         case 5:
                             scanf("%d",&paraCimaDireita);
-                            printf("\n");
-                            for(int i = 1; i <= paraCimaDireita; i++)
-                            {
-                                printf("Rainha move para cima e a direita\n");
-                            }
+                            movePeca(paraCimaDireita, "Rainha move para cima e a direita");
                             break;
                         case 6:
                             scanf("%d",&paraCimaEsquerda);
-                            printf("\n");
-                            for(int i = 1; i <= paraCimaEsquerda; i++)
-                            {
-                                printf("Rainha move para cima e a esquerda\n");
-                            }
+                            movePeca(paraCimaEsquerda, "Rainha move para cima e a esquerda");
                             break;
                         case 7:
                             scanf("%d",&paraBaixoDireita);
-                            printf("\n");
-                            for(int i = 1; i <= paraBaixoDireita; i++)
-                            {
-                                printf("Rainha move para baixo e a direita\n");
-                            }
+                            movePeca(paraBaixoDireita, "Rainha move para baixo e a direita");
                             break;
                         case 8:
                             scanf("%d",&paraBaixoEsquerda);
-                            printf("\n");
-                            for(int i = 1; i <= paraBaixoEsquerda; i++)
-                            {
-                                printf("Rainha move para baixo e a esquerda\n");
-                            }
+                            movePeca(paraBaixoEsquerda, "Rainha move para baixo e a esquerda");
                             break;
                         case 9:
-                            printf("Escolha outra peca.");
+                            printf("\nEscolha outra peca.\n");
                             opcaoMovimeto = 0;
                             opcaoPeca = 6;
                             break;           
@@ -178,39 +177,22 @@ int main() {
                     {
                         case 1:
                             scanf("%d",&paraCima);
-                            printf("\n");
-                            for(int i = 1; i <= paraCima; i++)
-                            {
-                                printf("Torre move para cima\n");
-                            }
-                                //opcaoMovimeto = 0;
+                            movePeca(paraCima, "Torre move para cima");
                             break;
                         case 2:
                             scanf("%d",&paraBaixo);
-                            printf("\n");
-                            for(int i = 1; i <= paraBaixo; i++)
-                            {
-                                printf("Torre move para baixo\n");
-                            }
+                            movePeca(paraBaixo, "Torre move para baixo");
                             break;
                         case 3:
                             scanf("%d",&paraDireita);
-                            printf("\n");
-                            for(int i = 1; i <= paraDireita; i++)
-                            {
-                                printf("Torre move para direita\n");
-                            }
+                            movePeca(paraDireita, "Torre move para a direita");
                             break;
                         case 4:
                             scanf("%d",&paraEsquerda);
-                            printf("\n");
-                            for(int i = 1; i <= paraEsquerda; i++)
-                            {
-                                printf("Rainha move para esqueda\n");
-                            }
+                            movePeca(paraEsquerda, "Torre move para a esquerda");
                             break;
                         case 5:
-                            printf("Escolha outra peca.");
+                            printf("\nEscolha outra peca.\n");
                             opcaoMovimeto = 0;
                             opcaoPeca = 6;
                             break;           
@@ -243,38 +225,22 @@ int main() {
                     {
                         case 1:
                             scanf("%d",&paraCimaDireita);
-                            printf("\n");
-                            for(int i = 1; i <= paraCimaDireita; i++)
-                            {
-                                printf("Bispo move para cima e a direita\n");
-                            }
+                            movePeca(paraCimaDireita, "Bispo move para cima e a direita");
                             break;
                         case 2:
                             scanf("%d",&paraCimaEsquerda);
-                            printf("\n");
-                            for(int i = 1; i <= paraCimaEsquerda; i++)
-                            {
-                                printf("Bispo move para cima e a esquerda\n");
-                            }
+                            movePeca(paraCimaEsquerda, "Bispo move para cima e a esquerda");
                             break;
                         case 3:
                             scanf("%d",&paraBaixoDireita);
-                            printf("\n");
-                            for(int i = 1; i <= paraBaixoDireita; i++)
-                            {
-                                printf("Bispo move para baixo e a direita\n");
-                            }
+                            movePeca(paraBaixoDireita, "Bispo move para baixo e a direita");
                             break;
                         case 4:
                             scanf("%d",&paraBaixoEsquerda);
-                            printf("\n");
-                            for(int i = 1; i <= paraBaixoEsquerda; i++)
-                            {
-                                printf("Bispo move para baixo e a esquerda\n");
-                            }
+                            movePeca(paraBaixoEsquerda, "Bispo move para baixo e a esquerda");
                             break;
                         case 5:
-                            printf("Escolha outra peca.");
+                            printf("\nEscolha outra peca.\n");
                             opcaoMovimeto = 0;
                             opcaoPeca = 5;
                             break;           
@@ -305,79 +271,39 @@ int main() {
                     switch (opcaoMovimeto)
                     {
                         case 1:
-                            paraCimaDireita = 2;
-                            printf("\n");
-                            for(int i = 1; i <= paraCimaDireita; i++)
-                            {
-                                printf("Cavalo move para cima\n");
-                            }
+                            movePeca(2, "Cavalo move para cima");
                             printf("Cavalo move para a direita\n");
                             break;
                         case 2:
-                            paraCimaEsquerda = 2;
-                            printf("\n");
-                            for(int i = 1; i <= paraCimaEsquerda; i++)
-                            {
-                                printf("Cavalo move para cima\n");
-                            }
+                            movePeca(2, "Cavalo move para cima");
                             printf("Cavalo move para a esquerda\n");
                             break;
                         case 3:
-                            paraDireitaCima = 2;
-                            printf("\n");
-                            for(int i = 1; i <= paraDireitaCima; i++)
-                            {
-                                printf("Cavalo move para a direita\n");
-                            }
+                            movePeca(2, "Cavalo move para a direita");
                             printf("Cavalo move para cima\n");
                             break;
                         case 4:
-                            paraDireitaBaixo = 2;
-                            printf("\n");
-                            for(int i = 1; i <= paraDireitaBaixo; i++)
-                            {
-                                printf("Cavalo move para a direita\n");
-                            }
+                            movePeca(2, "Cavalo move para a direita");
                             printf("Cavalo move para baixo\n");
                             break;
                         case 5:
-                            paraBaixoDireita = 2;
-                            printf("\n");
-                            for(int i = 1; i <= paraBaixoDireita; i++)
-                            {
-                                printf("Cavalo move para baixo\n");
-                            }
+                            movePeca(2, "Cavalo move para baixo");
                             printf("Cavalo move para a direita\n");
                             break;
                         case 6:
-                            paraBaixoEsquerda = 2;
-                            printf("\n");
-                            for(int i = 1; i <= paraBaixoEsquerda; i++)
-                            {
-                                printf("Cavalo move para baixo\n");
-                            }
+                            movePeca(2, "Cavalo move para baixo");
                             printf("Cavalo move para a esquerda\n");
                             break;
                         case 7:
-                            paraDireitaCima = 2;
-                            printf("\n");
-                            for(int i = 1; i <= paraDireitaCima; i++)
-                            {
-                                printf("Cavalo move para a direita\n");
-                            }
+                            movePeca(2, "Cavalo move para a direita");
                             printf("Cavalo move para cima\n");
                             break;
                         case 8:
-                            paraDireitaBaixo = 2;
-                            printf("\n");
-                            for(int i = 1; i <= paraDireitaBaixo; i++)
-                            {
-                                printf("Cavalo move para a direita\n");
-                            }
+                            movePeca(2, "Cavalo move para a direita");
                             printf("Cavalo move para baixo\n");
                             break;
                         case 9:
-                            printf("Escolha outra peca.");
+                            printf("\nEscolha outra peca.\n");
                             opcaoMovimeto = 0;
                             opcaoPeca = 6;
                             break;           
@@ -402,13 +328,5 @@ int main() {
 
     printf("Jogo encerrado.\n");
     
-
-    // Nível Mestre - Funções Recursivas e Loops Aninhados
-    // Sugestão: Substitua as movimentações das peças por funções recursivas.
-    // Exemplo: Crie uma função recursiva para o movimento do Bispo.
-
-    // Sugestão: Implemente a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.
-    // Inclua o uso de continue e break dentro dos loops.
-
     return 0;
 }
